08fd491dca2f7e1bba204ec489fd9496cf2dc84e
L7GABRIELZIN <165018281+L7GABRIELZIN@users.noreply.github.com>
2024-03-26 08:24:36 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,6 @@
+basic.forever(function () {
+    basic.showString("oi me chamo gabriel")
+    basic.showIcon(IconNames.Ghost)
+    basic.showString("tenho 13 anos")
+    music.play(music.stringPlayable("D B G D C A C C5 ", 120), music.PlaybackMode.UntilDone)
+})
